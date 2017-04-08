a356de370b8f376ede040a90373f9feb1b1d5afd
JJ <JJ@JJs-MBP.local>
2017-04-08 18:25:04 +0700

    Add get todo by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -19,6 +20,33 @@ app.post('/todos', (req, res) => {
 	}, (err) => {
 		res.status(400).send(err);
 	});
+});
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (err) => {
+		res.status(400).send(err);
+	});
+});
+
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send('Invalid ID.');
+	}
+	
+	Todo.findById(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send('ID not found.');
+		} 
+			res.send({todo});
+		
+	}).catch((err) => {
+		res.status(400).send('Could not find todo.');
+	})
 });
 
 app.listen(3000, () => {
